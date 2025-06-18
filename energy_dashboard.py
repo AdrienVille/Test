fc84fa4097feb64918cc69f2d930a7e3d4760573
avgjy <adrien.ville@guujiya-ingenierie.com>
2025-06-18 11:33:25 +0200

    Update energy_dashboard.py

--- energy_dashboard.py
+++ energy_dashboard.py
@@ -10,8 +10,7 @@ import statsmodels.api as sm
 from reportlab.lib.pagesizes import letter
 from reportlab.pdfgen import canvas
 from reportlab.lib.utils import ImageReader
-import os
-import scipy
+
 
 
 def parse_excel(contents, filename, header=0):
